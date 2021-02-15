9cb1d43915927cdebcd8b81189da9ff2f79444f4
Daniel Cherino <daniel@cherino.me>
2021-02-15 00:26:59 +0000

    Get DKIM and SPF from Authentication-Results header

--- server/parse.js
+++ server/parse.js
@@ -1,7 +1,7 @@
 /**
  * React Dmarc Sift. DAG's 'Parse' node implementation
-*/
-'use strict';
+ */
+"use strict";
 
 // Javascript nodes are run in a Node.js sandbox so you can require dependencies following the node paradigm
 // e.g. var moment = require('moment');
@@ -11,37 +11,70 @@ module.exports = function (got) {
   // inData contains the key/value pairs that match the given query
   const inData = got.in;
 
-  console.log('email-sift-web: parse.js: running...');
+  console.log("email-sift-web: parse.js: running...");
 
   const results = inData.data.map(({ value: valueBuffer }) => {
     // Parse the JMAP information for each message more info here: https://docs.redsift.com/docs/server-code-jmap
     const emailJmap = JSON.parse(valueBuffer);
-    const { id, threadId, subject, textBody, strippedHtmlBody } = emailJmap;
+    const {
+      id,
+      threadId,
+      headers,
+      name,
+      from,
+      cc,
+      bcc,
+      subject,
+      date,
+      textBody,
+      strippedHtmlBody,
+    } = emailJmap;
+
+    // Headers
+    const dkim = headers["Authentication-Results"]
+      ? headers["Authentication-Results"].match(/(dkim=)\w+/g)
+      : null;
+    const spf = headers["Authentication-Results"]
+      ? headers["Authentication-Results"].match(/(spf=)\w+/g)
+      : null;
+    const headerData = {
+      DKIM: dkim,
+      SPF: spf,
+    };
 
     // Not all emails contain a textBody so we do a cascade selection
-    const body = textBody || strippedHtmlBody || '';
+    const body = textBody || strippedHtmlBody || "";
     const wordCount = countWords(body);
 
     const key = `${threadId}/${id}`;
     const value = {
       id,
+      name,
+      from,
+      cc,
+      bcc,
+      date,
       body,
+      headerData,
       subject,
       threadId,
-      wordCount
+      wordCount,
     };
 
     // Emit into "messages-st" store so count can be calculated by the "Count" node
     // Emit information on the "messages" output so we can display them in the email list and detail
-    return [{
-      key,
-      value,
-      name: 'messages-st'
-    }, {
-      key,
-      value,
-      name: 'messages'
-    }];
+    return [
+      {
+        key,
+        value,
+        name: "messages-st",
+      },
+      {
+        key,
+        value,
+        name: "messages",
+      },
+    ];
   });
 
   // Possible return values are: undefined, null, promises, single or an array of objects
@@ -58,8 +91,8 @@ module.exports = function (got) {
  * Simple function to count number of words in a string
  */
 function countWords(body) {
-  let s = body.replace(/\n/gi, ' ');
-  s = s.replace(/(^\s*)|(\s*$)/gi, '');
-  s = s.replace(/[ ]{2,}/gi, '');
-  return s.split(' ').length;
+  let s = body.replace(/\n/gi, " ");
+  s = s.replace(/(^\s*)|(\s*$)/gi, "");
+  s = s.replace(/[ ]{2,}/gi, "");
+  return s.split(" ").length;
 }
