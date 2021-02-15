ca72df3e9a828c702d918e5c3a0d71360a302003
Daniel Cherino <daniel@cherino.me>
2021-02-15 14:24:01 +0000

    Moving types to an external file

--- frontend/src/scripts/components/App.tsx
+++ frontend/src/scripts/components/App.tsx
@@ -1,35 +1,25 @@
 /* eslint no-console: 0 */
 
 import React from 'react';
-// import { translate } from 'react-i18next';
 import { useTranslation } from 'react-i18next';
-interface Message {
-  subject: string;
-}
-interface Data {
-  counts: {
-    messageTotal: number;
-    wordTotal: number;
-    wpmTotal: number;
-  };
-  messages: Array<Message>;
-}
-interface AppProps {
-  data: Data;
-}
+import { AppProps } from '../types'
 
 const App = ({ data }: AppProps) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation(['app']);
   const { counts, messages } = data;
+
   return (
     <div>
       <h1>{t('app:title-home')}</h1>
       <h4>{t('app:description-home', { count: counts.messageTotal })}</h4>
-      {messages.map((m, key) => (
+
+      {messages.map((m, key) => {
+        console.log(m)
+        return (
         <div key={key}>
           <p>{m.subject}</p>
         </div>
-      ))}
+      )})}
     </div>
   );
 };
