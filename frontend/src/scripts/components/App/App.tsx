f2363d4303123de1d4d6c050e4f693dd76da4e21
Daniel Cherino <daniel@cherino.me>
2021-02-16 03:31:10 +0000

    Adding Analytic component into the main component

--- frontend/src/scripts/components/App/App.tsx
+++ frontend/src/scripts/components/App/App.tsx
@@ -7,12 +7,14 @@ import EmailList from '../EmailList/EmailList';
 import { GridContainer } from './App.styles';
 import Title from '../Title/Title';
 import Footer from '../Footer/Footer';
+import Analytics from '../Analytics/Analytics';
 
 const App = ({ data }: AppProps) => {
   return (
     <GridContainer>
       <Header />
       <Title counts={data.counts} />
+      <Analytics data={data} />
       <EmailList data={data} />
       <Footer />
     </GridContainer>
