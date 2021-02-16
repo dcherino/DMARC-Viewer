f8cffa3196e24e62df37c6fd2be9a776482e388e
Daniel Cherino <daniel@cherino.me>
2021-02-16 08:44:08 +0000

    Creating Footer component and styles

---
+++ frontend/src/scripts/components/Footer/Footer.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.footer`
+  grid-area: Footer;
+  text-align: center;
+  font-family: 'Lato', sans-serif;
+  font-size: 1rem;
+  color: #555;
+  padding: 2rem;
+`;
---
+++ frontend/src/scripts/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FooterWrapper } from './Footer.styles';
+
+const Footer = () => {
+  return <FooterWrapper>&copy; 2021 Copyright Red Sift </FooterWrapper>;
+};
+
+export default Footer;
